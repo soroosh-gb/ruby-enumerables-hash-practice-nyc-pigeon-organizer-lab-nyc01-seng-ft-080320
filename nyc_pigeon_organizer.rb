b0f5bd72517c27eb262c1eaaf961789e488557ae
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-07-07 20:18:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   
   data.each_with_object({}) do |(key , value) , final_array|
+    binding.pry
     value.each do |inner_key , names|
       names.each do |names|
         if !final_array[names]
